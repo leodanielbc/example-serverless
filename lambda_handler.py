d85300752630f47b9acafff836ee102e15dabd6d
Leonardo BENITEZ <leonardo.benitez@naranja.com>
2023-06-09 23:59:05 -0500

    add funtion resta, config automation deploy, config serverless

--- lambda_handler.py
+++ lambda_handler.py
@@ -11,6 +11,16 @@ def suma(event, context):
         "message": "ok",
         "suma": operando_b + operando_a
     }
+def resta(event, context):
+    operando_a = event['operando_a']
+    operando_b = event['operando_b']
+    if not isinstance(operando_a, int) or not isinstance(operando_b, int):
+        raise Exception('Parametros no son numeros enteros')
+    return {
+        "status": 200,
+        "message": "ok",
+        "suma": operando_b - operando_a
+    }
 
 
 def fecha_actual(event, context):
@@ -19,4 +29,3 @@ def fecha_actual(event, context):
         "message": "ok",
         "suma": datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%s')
     }
-
